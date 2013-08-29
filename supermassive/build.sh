4c0cbcdd69491accf9c01b21be0ff1704e32301d
Amit <amit.joshi@moovweb.com>
2013-08-29 16:16:19 -0700

    Making build script work across platforms. Ignoring executables

--- supermassive/build.sh
+++ supermassive/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-gcc writer.c util.c -o writer
-gcc reader.c util.c -o reader
+OS_NAME=`uname`
+if [ "$OS_NAME" == "Linux" ]; then
+	gcc writer.c util.c -o writer -lrt
+	gcc reader.c util.c -o reader -lrt
+else
+	gcc writer.c util.c -o writer
+	gcc reader.c util.c -o reader
+fi
+
 
 
 
